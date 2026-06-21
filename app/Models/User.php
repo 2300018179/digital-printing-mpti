ae7003da3b67421711de94a0f08e9d1051dfe1b5
dyahrahmaa <2300018154@webmail.uad.ac.id>
2026-06-21 08:06:58 +0700

    penambahan

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,6 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-// 👇 PERBAIKAN DI SINI: Tambahkan 'role' (dan 'phone' jika ada)
 #[Fillable(['name', 'email', 'password', 'role', 'phone'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
